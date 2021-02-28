eea209cc5c94ffb384b424acd9efccc197c18eb6
James Karsten <james.e.karsten@gmail.com>
2021-02-28 03:26:22 -0500

    Creates Claremont CSV file with calculations

--- csvReader.py
+++ csvReader.py
@@ -2,6 +2,7 @@ import csv
 
 dataPlacesURL = 'https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-place-totals.csv'
 
+claremontData = []
 
 with open('2-28-latimes-place-totals.csv', newline = '') as dataPlacesCSV:
     # Initializes reader
@@ -13,12 +14,40 @@ with open('2-28-latimes-place-totals.csv', newline = '') as dataPlacesCSV:
         headerD[headerRow[col]] = col
     colID, colDate, colCases, colPopulation = headerD['id'], headerD['date'], headerD['confirmed_cases'], headerD['population']
     
-    # Creates/overwrites Claremont cases CSV file
-    with open('claremont-cases.csv', 'w', newline='') as dataClaremontCSV:
-        # Initializes writer
-        claremontWriter = csv.writer(dataClaremontCSV, delimiter=',')
-        claremontWriter.writerow(['date', 'cases', 'new_cases', 'population', 'cases_per_100k'])
-        # Iterates through CSV
-        for row in placesReader:
-            if row[colID] == 'City of Claremont':
-                claremontWriter.writerow([row[colDate], row[colCases], None, row[colPopulation], None])
+    # Iterates through CSV and appends select columns from each Claremont row to list
+    for row in placesReader:
+        if row[colID] == 'City of Claremont':
+            claremontData.append([row[colDate], int(row[colCases]), None, int(row[colPopulation])])
+
+# Reverses list of Claremont cases
+claremontData.reverse()
+
+# Overwrites (or creates) Claremont cases CSV file through a list
+with open('claremont-cases.csv', 'w', newline='') as dataClaremontCSV:
+    # Initializes writer and writes header row
+    claremontWriter = csv.writer(dataClaremontCSV, delimiter=',')
+    claremontWriter.writerow(['date', 'cases', 'new_cases', 'cases_per_10k', 'cases_last_14_days', 'avg_cases_last_14_days', 'population'])
+
+    # Initializes calculation variables
+    previousDayCases = 0
+    previous14DayCases = 0
+
+    # Copies values from list to CSV file while doing calculations
+    for i in range(len(claremontData)):
+        # Sets calculation variables
+        claremontData[i][2] = claremontData[i][1] - previousDayCases
+        previous14DayCases += claremontData[i][2]
+        if i >= 14: previous14DayCases -= claremontData[i-14][2]
+        
+        # Writes row of CSV file
+        claremontWriter.writerow([claremontData[i][0],                                          # date
+                                  claremontData[i][1],                                          # cases
+                                  claremontData[i][2],                                          # new cases
+                                  round(claremontData[i][1] / claremontData[i][3] * 10000),     # cases per 10k
+                                  previous14DayCases,                                           # cases in the last 14 days
+                                  round(previous14DayCases / 14),                               # average cases in the last 14 days
+                                  claremontData[i][3]])                                         # population
+        
+        # Sets previous day cases variable
+        previousDayCases = claremontData[i][1]
+        
