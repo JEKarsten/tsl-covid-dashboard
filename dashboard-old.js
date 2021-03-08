999962ee8a5233943f5cae6ffffda2cbc6d431f7
James Karsten <james.e.karsten@gmail.com>
2021-03-08 00:32:27 -0500

    fixed charts to be responsive and switchable

--- dashboard-old.js
+++ dashboard-old.js
@@ -8,20 +8,6 @@ var TSL_MALIBU_TRANSLUCENT = 'rgba(105, 179, 227, .50)';
 var DATA_ARRAY = [];
 var DATA_ARRAY_30 = [];
 
-var DATES_ALL_TIME = [];
-var CASES_TOTAL_ALL_TIME = [];
-var CASES_NEW_ALL_TIME = [];
-var CASES_14_AVG_ALL_TIME = [];
-
-var DATES_30_DAYS = [];
-var CASES_TOTAL_30_DAYS = [];
-var CASES_NEW_30_DAYS = [];
-var CASES_14_AVG_30_DAYS = [];
-
-var UNIT_ALL_TIME = 'month';
-var UNIT_30_DAYS = 'day';
-
-var CHART;
 
 $.ajax({
     type: 'GET',
@@ -31,68 +17,62 @@ $.ajax({
 });
 
 
-$("#month").click(function() {
-    CHART.options.scales.xAxes[0].time.unit = UNIT_30_DAYS;
-    var data = CHART.config.data;
-    data.labels = DATES_30_DAYS;
-    data.datasets[0].data = CASES_TOTAL_30_DAYS;
-    data.datasets[1].data = CASES_NEW_30_DAYS;
-    data.datasets[2].data = CASES_14_AVG_30_DAYS;
-    CHART.update();
-});
-$("#all-time").click(function() {
-    CHART.options.scales.xAxes[0].time.unit = UNIT_ALL_TIME;
-    var data = CHART.config.data;
-    data.labels = DATES_ALL_TIME;
-    data.datasets[0].data = CASES_TOTAL_ALL_TIME;
-    data.datasets[1].data = CASES_NEW_ALL_TIME;
-    data.datasets[2].data = CASES_14_AVG_ALL_TIME;
-    CHART.update();
-});
-
-
-
 function processData(allText) {
-    var allRows = allText.split(/\r\n|\n/);
-    var headers = allRows[0].split(',');
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
 
-    for (var i = 1; i < allRows.length; i++) {
-        var row = allRows[i].split(',');
-        if (row.length == headers.length) {
+    for (var i = 1; i < allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
 
-            currentCasesNew = row[2];
-            if (currentCasesNew < 0) { currentCasesNew = null; }
-
-            DATES_ALL_TIME.push(row[0]);
-            CASES_TOTAL_ALL_TIME.push(row[1]);
-            CASES_NEW_ALL_TIME.push(currentCasesNew);
-            CASES_14_AVG_ALL_TIME.push(row[5]);
-
-            if (i >= allRows.length - 31) {
-                DATES_30_DAYS.push(row[0]);
-                CASES_TOTAL_30_DAYS.push(row[1]);
-                CASES_NEW_30_DAYS.push(currentCasesNew);
-                CASES_14_AVG_30_DAYS.push(row[5]);
+            var day = [];
+            for (var j=0; j<headers.length; j++) {
+                day.push(data[j]);
             }
-
-
-
+            DATA_ARRAY.push(day);
+            if (i >= allTextLines.length - 31) {
+                DATA_ARRAY_30.push(day);
+            }
         }
     }
 
-    /* console.log(DATES_ALL_TIME);
-    console.log(CASES_TOTAL_ALL_TIME);
-    console.log(CASES_NEW_ALL_TIME);
-    console.log(CASES_14_AVG_ALL_TIME);
-    console.log(DATES_30_DAYS);
-    console.log(CASES_TOTAL_30_DAYS);
-    console.log(CASES_NEW_30_DAYS);
-    console.log(CASES_14_AVG_30_DAYS); */
-    makeChart(UNIT_30_DAYS, DATES_30_DAYS, CASES_TOTAL_30_DAYS, CASES_NEW_30_DAYS, CASES_14_AVG_30_DAYS);
+    console.log(DATA_ARRAY);
+    console.log(DATA_ARRAY_30);
+    makeChart('chart-month', DATA_ARRAY_30, 'day');
+    makeChart('chart-all-time', DATA_ARRAY, 'month');
 }
 
-function makeChart(timeUnit, dates, casesTotal, casesNew, cases14Avg) {
-    CHART = new Chart('chart-month', {
+
+document.getElementById('month').addEventListener('click', function() {
+    document.getElementById('chart-all-time').style.visibility = 'hidden';
+    document.getElementById('chart-month').style.visibility = 'visible';
+});
+
+document.getElementById('all-time').addEventListener('click', function() {
+    document.getElementById('chart-month').style.visibility = 'hidden';
+    document.getElementById('chart-all-time').style.visibility = 'visible';
+});
+
+
+function makeChart(canvasName, data, timeUnit) {
+
+    var dates = new Array();
+    var casesTotal = new Array();
+    var casesNew = new Array();
+    var cases14Avg = new Array();
+
+    for (var i = 0; i < data.length; i++) {
+        currentDate = data[i][0];
+        currentCasesTotal = data[i][2];
+        if (currentCasesTotal < 0) { currentCasesTotal = null; }
+
+        dates.push(currentDate);
+        casesTotal.push(data[i][1]);
+        casesNew.push(currentCasesTotal);
+        cases14Avg.push(data[i][5]);
+    }
+
+    var chart = new Chart(canvasName, {
         type: 'line',
         data: {
             labels: dates,
@@ -174,10 +154,10 @@ function makeChart(timeUnit, dates, casesTotal, casesNew, cases14Avg) {
                 displayColors: false,
                 callbacks: {
                     label: function(tooltipItem, data) {
-                        var totalValue = data.datasets[0].data[tooltipItem.index];
-                        var newValue = data.datasets[1].data[tooltipItem.index];
-                        totalValue = totalValue.toString().split(/(?=(?:...)*$)/).join(',');
-                        if (newValue != null) { newValue = newValue.toString().split(/(?=(?:...)*$)/).join(','); }
+						var totalValue = data.datasets[0].data[tooltipItem.index];
+						var newValue = data.datasets[1].data[tooltipItem.index];
+						totalValue = totalValue.toString().split(/(?=(?:...)*$)/).join(',');
+						newValue = newValue.toString().split(/(?=(?:...)*$)/).join(',');
                         return ['Total cases: ' + totalValue, 'New cases: ' + newValue];
                     },
                 }
@@ -188,5 +168,8 @@ function makeChart(timeUnit, dates, casesTotal, casesNew, cases14Avg) {
             }
         },
     });
-    CHART.render();
+
+    chart.render();
+
+    console.log(chart);
 }
